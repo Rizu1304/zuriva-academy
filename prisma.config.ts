d4a1c3732e07693e7419139c01defbc7f9bfca48
Rinor <rinor.zukaj@hotmail.ch>
2026-03-24 00:59:21 +0100

    Initial commit

---
+++ prisma.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "prisma/config"
+
+export default defineConfig({
+  datasource: {
+    url: "postgresql://postgres:Zuriva2026!@db.ggtkqiuxgxrdlkupeyml.supabase.co:6543/postgres?pgbouncer=true",
+  },
+})
